35eeb814e5d72156275d1e136260b3303f3d0db2
Dan Alvarez <dan.alvarez@peerfit.com>
2022-03-27 16:13:24 -0600

    try and revert to working model

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const express = require('express');
 const cors = require('cors');
 
 const getAllUsers = require('./getAllRoute');
-const getUserById = require('./getUserByIdRoute');
-const createUser = require('./createUserRoute');
+// const getUserById = require('./getUserByIdRoute');
+// const createUser = require('./createUserRoute');
 
 const server = express();
 const port = process.env.PORT || 8000;
@@ -17,8 +17,8 @@ server.get('/', (req, res) => {
   res.send('<h1>This is the Birdies for Books Api</h1>')
 });
 server.use('/users', getAllUsers);
-server.use('/users/:id', getUserById);
-server.use('/users', createUser); // not working
+// server.use('/users/:id', getUserById);
+// server.use('/users', createUser); // not working
 // server.put('/users/:id', updateUser );
 // server.delete('/users/:id', deleteUser);
 
