cf23872f46bdc764259da56d27dbca4945ef12bf
Dan Alvarez <dan.alvarez@peerfit.com>
2022-03-27 10:40:47 -0600

    remove console.log

--- getUSerByIdRoute.js
+++ getUSerByIdRoute.js
@@ -4,7 +4,6 @@ const Users = require('./model')
 router.get('/', async (req, res) => {
   const id = parseInt(req.params.id)
   const users = await Users.findAll()
-  console.log('id', id, )
   const user = users.find(user => user.user_id == id)
   res.json(user)
 });
