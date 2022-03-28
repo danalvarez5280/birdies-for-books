1e3786a339f4da8617db04c16c1d2b6e38ede3c6
Dan Alvarez <dan.alvarez@peerfit.com>
2022-03-27 18:07:44 -0600

    add post for users

--- index.js
+++ index.js
@@ -26,45 +26,11 @@ server.get('/users/:id', async (req, res) => {
   res.json(individualUser);
 });
 
-// server.post('/users', async (req, res) => {
-//   const newUser = req.body
-//   console.log('newUser', newUser);
-//   console.log('req body', req.body);
+server.post('/users', async (req, res) => {
+  const newUser = req.body;
+  await Users.addUser(newUser, res);
+});
 
-//   const users = await Users.findAll();
-//   users.push(newUser);
-//   res.json(newUser);
-// });
-
-
-// server.post('/users', (request, response) => {
-//   const user = request.body;
-
-//   for (let requiredParameter of ['name', 'email', 'phone_number', 'pledge_score', 'pledge_amount', 'score_amount']) {
-//     if (!user[requiredParameter]) {
-//       return response
-//         .status(422)
-//         .send({
-//           error: `Expected format: {
-//         name: <String>,
-//         email: <String>,
-//         phone_number: <String>,
-//         pledge_score: <String>,
-//         pledge_amount: <String>,
-//         score_amount: <String>,
-//       }. You're missing a "${requiredParameter}" property.`
-//         });
-//     }
-//   }
-
-//   database('users').insert(user, '*')
-//     .then(user => {
-//       response.status(201).json(user)
-//     })
-//     .catch(error => {
-//       response.status(500).json({ error });
-//     });
-// });
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`)
--- model.js
+++ model.js
@@ -4,6 +4,21 @@ const findAll = () => {
   return db('users');
 };
 
+const addUser = (newUser, res) => {
+  if (!newUser) {
+    return res.status(422).send({ error: `Expoected a new user, please add a user` });
+  }
+
+  return db('users').insert(newUser, '*')
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
+};
+
 module.exports = {
-  findAll
+  findAll,
+  addUser
 };
